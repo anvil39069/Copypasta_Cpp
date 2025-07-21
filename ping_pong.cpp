696acf4d5262aa815572992ee647e175e2a825dc
anvil39069 <anvilsky123tg@gmail.com>
2025-07-21 17:43:12 +0545

    updating

--- ping_pong.cpp
+++ ping_pong.cpp
@@ -12,11 +12,14 @@ int main(){
     SetTargetFPS(60);
     while(WindowShouldClose()==false){
         BeginDrawing();
-
+        //DRAWING
+        DrawCircle(screen_width/2,screen_height/2,20,WHITE);
+        DrawRectangle(10,screen_height/2 -60,25,120,WHITE);
+        DrawRectangle(screen_width-35,screen_height/2 -60,25,120,WHITE);
         EndDrawing();
     }
 
     CloseWindow();
     return 0;
-
+    
 }
