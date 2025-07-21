310a8c1194fdb2ec482c52cbe9b063f3972b29a4
anvil39069 <anvilsky123tg@gmail.com>
2025-07-21 18:36:09 +0545

    Added the ball movement and the box movement but they are not working  accordingly

--- ping_pong.cpp
+++ ping_pong.cpp
@@ -1,25 +1,106 @@
-#include<iostream>
-#include"raylib.h"
+#include <iostream>
+#include "raylib.h"
 using namespace std;
 
-int main(){
+class Ball
+{
+public:
+    float x, y;
+    int speed_x, speed_y;
+    int radius;
 
-    cout<<"starting the game"<<endl;
-    const int screen_width=1200;
-    const int screen_height=800;
+    void Draw()
+    {
+        DrawCircle(x, y, radius, WHITE);
+    }
+    void update()
+    {
 
-    InitWindow(screen_width,screen_height,"the pong game!");
+        x += speed_x;
+        y += speed_y;
+
+        if (y + radius >= GetScreenHeight() || y - radius <= 0)
+        {
+            speed_y *= -1;
+        }
+        if (x + radius >= GetScreenHeight() || x - radius <= 0)
+        {
+            speed_x *= -1;
+        }
+    }
+};
+class Paddle
+{
+public:
+    float x, y;
+    float width, height;
+    int speed;
+    void Draw()
+    {
+        DrawLine(x, y, width, height, WHITE);
+    }
+    void update()
+    {
+        if (IsKeyDown(KEY_UP))
+        {
+            y = y - speed;
+        }
+        if (IsKeyDown(KEY_DOWN))
+        {
+            y = y + speed;
+        }
+        if (y <= 0)
+        {
+            y = 0;
+        }
+        if (y + height >= GetScreenHeight())
+        {
+            y = GetScreenHeight() - height;
+        }
+    }
+};
+
+Ball ball;
+Paddle player;
+
+int main()
+{
+
+    cout << "starting the game" << endl;
+    const int screen_width = 1200;
+    const int screen_height = 800;
+
+    InitWindow(screen_width, screen_height, "the pong game!");
     SetTargetFPS(60);
-    while(WindowShouldClose()==false){
+    ball.radius = 20;
+    ball.x = screen_width / 2;
+    ball.y = screen_height / 2;
+    ball.speed_x = 7;
+    ball.speed_y = 7;
+
+    player.width = 25;
+    player.height = 120;
+    player.x = screen_width - player.width - 10;
+    player.y = screen_height / 2 - player.height / 2;
+    player.speed = 6;
+
+    while (WindowShouldClose() == false)
+    {
         BeginDrawing();
-        //DRAWING
-        DrawCircle(screen_width/2,screen_height/2,20,WHITE);
-        DrawRectangle(10,screen_height/2 -60,25,120,WHITE);
-        DrawRectangle(screen_width-35,screen_height/2 -60,25,120,WHITE);
+
+        // updating
+        ball.update();
+        player.update();
+
+        // DRAWING
+        ClearBackground(BLACK);
+        DrawLine(screen_width / 2, 0, screen_width / 2, screen_height, WHITE);
+        ball.Draw();
+        DrawRectangle(10, screen_height / 2 - 60, 25, 120, WHITE);
+        DrawRectangle(screen_width - 35, screen_height / 2 - 60, 25, 120, WHITE);
         EndDrawing();
     }
 
     CloseWindow();
     return 0;
-    
 }
