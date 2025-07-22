f905219110666b754dd0e2be3b8dc19a977228cf
anvil39069 <anvilsky123tg@gmail.com>
2025-07-22 17:41:56 +0545

    not workiing as intended

--- ping_pong.cpp
+++ ping_pong.cpp
@@ -2,6 +2,15 @@
 #include "raylib.h"
 using namespace std;
 
+Color Green=Color{38,185,154,255};
+Color Dark_Green=Color{20,160,133,255};
+Color Light_Green=Color{129,204,184,255};
+Color Yellow=Color{243,213,91,255};
+
+int player_score=0;
+int cpu_score=0;
+
+
 class Ball
 {
 public:
@@ -11,7 +20,7 @@ public:
 
     void Draw()
     {
-        DrawCircle(x, y, radius, WHITE);
+        DrawCircle(x, y, radius, Yellow);
     }
     void update()
     {
@@ -23,21 +32,55 @@ public:
         {
             speed_y *= -1;
         }
-        if (x + radius >= GetScreenHeight() || x - radius <= 0)
+
+        if (x + radius >= GetScreenHeight()) //cpu  wins
         {
-            speed_x *= -1;
+            cpu_score++;
+            Resetball();
+        }
+        
+        if(x - radius <= 0)
+        {
+           player_score++;
+           Resetball();
         }
     }
+    void Resetball(){
+        x=GetScreenWidth()/2;
+        y=GetScreenHeight()/2;
+        int speed_choices[2]={-1,1};
+        speed_x*=speed_choices[GetRandomValue(0,1)];
+        speed_y*=speed_choices[GetRandomValue(0,1)];
+    }
+
+
+
+
+
+
 };
 class Paddle
 {
+    protected:
+
+        void LimitMovement()
+        {
+        if (y <= 0)
+        {
+            y = 0;
+        }
+        if (y + height >= GetScreenHeight())
+        {
+            y = GetScreenHeight() - height;
+        }
+    }
 public:
     float x, y;
     float width, height;
     int speed;
     void Draw()
     {
-        DrawLine(x, y, width, height, WHITE);
+        DrawRectangleRounded(Rectangle{x, y, width, height},0.8,0,WHITE);
     }
     void update()
     {
@@ -49,29 +92,39 @@ public:
         {
             y = y + speed;
         }
-        if (y <= 0)
-        {
-            y = 0;
-        }
-        if (y + height >= GetScreenHeight())
-        {
-            y = GetScreenHeight() - height;
-        }
+        LimitMovement();
     }
+};
+class Cpupaddle:public Paddle{
+    public:
+
+    void update(int ball_y)
+    {
+        if(y+height/2 > ball_y){
+            y=y-speed;
+        }
+        if(y+height/2 <=ball_y)
+        {
+            y=y+speed;
+        }
+        LimitMovement();
+    }
+
 };
 
 Ball ball;
 Paddle player;
+Cpupaddle cpu;
 
 int main()
 {
 
     cout << "starting the game" << endl;
-    const int screen_width = 1200;
+    const int screen_width = 1400;
     const int screen_height = 800;
-
     InitWindow(screen_width, screen_height, "the pong game!");
     SetTargetFPS(60);
+
     ball.radius = 20;
     ball.x = screen_width / 2;
     ball.y = screen_height / 2;
@@ -84,6 +137,12 @@ int main()
     player.y = screen_height / 2 - player.height / 2;
     player.speed = 6;
 
+    cpu.height=120;
+    cpu.width=25;
+    cpu.x=10;
+    cpu.y=screen_height/2-cpu.height/2;
+    cpu.speed=6;
+
     while (WindowShouldClose() == false)
     {
         BeginDrawing();
@@ -91,13 +150,30 @@ int main()
         // updating
         ball.update();
         player.update();
+        cpu.update(ball.y);
+
+        //checking for collision
+        if(CheckCollisionCircleRec(Vector2{ball.x,ball.y},ball.radius, Rectangle{player.x,player.y,player.width,player.height}))
+        {
+            ball.speed_x*=-1;
+        }
+        
+        if(CheckCollisionCircleRec(Vector2{ball.x,ball.y,},ball.radius,Rectangle{cpu.x,cpu.y,cpu.width,cpu.height})){
+            ball.speed_x*=-1;
+
+        }
 
         // DRAWING
-        ClearBackground(BLACK);
+        ClearBackground(Dark_Green);
+        DrawRectangle(screen_width/2,0,screen_width/2,screen_height,Green);
+        DrawCircle(screen_width/2,screen_height/2,150,Light_Green);
         DrawLine(screen_width / 2, 0, screen_width / 2, screen_height, WHITE);
         ball.Draw();
-        DrawRectangle(10, screen_height / 2 - 60, 25, 120, WHITE);
-        DrawRectangle(screen_width - 35, screen_height / 2 - 60, 25, 120, WHITE);
+        cpu.Draw();
+        player.Draw();
+        DrawText(TextFormat("%i",cpu_score),screen_width/4-20,20,80,WHITE);
+        DrawText(TextFormat("%i",player_score),3*screen_width/4-20,20,80,WHITE);
+
         EndDrawing();
     }
 
